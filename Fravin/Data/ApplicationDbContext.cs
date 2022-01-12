3bf5d852e7a9a1072bea85fddc59e9fe8ec307cd
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-01-12 09:31:07 +0200

    Add project files.

---
+++ Fravin/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Fravin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fravin.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+               
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
