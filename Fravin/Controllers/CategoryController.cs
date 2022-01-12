b0229864f6a3fd9fbc89cee9c5967d82daa8653a
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-01-12 10:52:25 +0200

    Main Category Update

--- Fravin/Controllers/CategoryController.cs
+++ Fravin/Controllers/CategoryController.cs
@@ -65,5 +65,41 @@ namespace Fravin.Controllers
             return View(obj);
 
         }
+        //Get - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //Post - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            //if (obj==null)
+            //{
+            // return NotFound();   
+            //}
+            // 
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
     }
 }
